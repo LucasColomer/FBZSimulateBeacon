2a96b88c7f3d6eaafd67a1b1bc26af7502362d8c
Lucas Colomer <lco@frianbiz.com>
2017-02-16 13:13:59 +0100

    Initial Commit

---
+++ FBZSimulateBeacon/FBZSimulateBeacon.h
@@ -0,0 +1,19 @@
+//
+//  FBZSimulateBeacon.h
+//  FBZSimulateBeacon
+//
+//  Created by lucas colomer on 16/02/2017.
+//  Copyright © 2017 lucas colomer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBZSimulateBeacon.
+FOUNDATION_EXPORT double FBZSimulateBeaconVersionNumber;
+
+//! Project version string for FBZSimulateBeacon.
+FOUNDATION_EXPORT const unsigned char FBZSimulateBeaconVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBZSimulateBeacon/PublicHeader.h>
+
+
